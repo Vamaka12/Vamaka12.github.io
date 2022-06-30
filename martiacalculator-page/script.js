e590559d49df248cfe894c868b8042ff9f22d871
Vamaka12 <71032898+Vamaka12@users.noreply.github.com>
2022-06-30 21:14:41 +0400

    Add files via upload

--- martiacalculator-page/script.js
+++ martiacalculator-page/script.js
@@ -1,17 +1,19 @@
 jQuery('document').ready(function () {
 
+
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
 
     xhr.addEventListener("readystatechange", function() {
     if(this.readyState === 4) {
-        console.log(this.responseText);
-        var releasepower = this.response.text;
+        releasepower = JSON.parse(this.responseText).power
+        console.log(releasepower);
+
     }
     });
 
     xhr.open("GET", "https://api.mars.cards/api/strength/martia-release-power");
-    xhr.setRequestHeader("authorization", "");
+    xhr.setRequestHeader("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzeW1ib2wiOiJmNDcwNjIxNi05OGYwLTQzZGUtOTI3MC1jN2RkYzIzYzk2YjAiLCJ1c2VybmFtZSI6InZhbWFrYTEyIiwiaWF0IjoxNjU2NTY1OTcyLCJleHAiOjE2NTY1ODM5NzJ9.4LGtulrmBJ2KYZPbetFv-YXFiRmMPJjWt5edjjcmurw");
 
     xhr.send();
 
@@ -20,12 +22,23 @@ jQuery('document').ready(function () {
     })
 })
 
-
+var releasepower = 0;
 
 function calculate(){
     var a = jQuery('#strength').val();
-    var calc = a * releasepower;
-    c = calc.toString().split(".")[1].slice(0, 4);
-    calc = calc.toString().split(".")[0] + "." + c;
-    jQuery("#label").text("You will earn about " + calc + " martia per hour");
+    var calc = (85 * releasepower) / 100;
+    // var calc = (85 * v) / 100;
+
+    console.log(calc);
+    calc = calc * a;
+    try {
+        c = calc.toString().split(".")[1].slice(0, 4);
+        calc = calc.toString().split(".")[0] + "." + c;
+        jQuery("#label").text("You will earn more than " + calc + " martia per hour");
+    }
+    catch(e){
+
+    }
+
+    
 }
